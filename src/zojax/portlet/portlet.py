d496931f90776ad92f58212c5ad33ee096cc12e4
Anatoly Bubenkov <bubenkoff@gmail.com>
2011-01-05 16:17:12 +0000

    fix login

--- src/zojax/portlet/portlet.py
+++ src/zojax/portlet/portlet.py
@@ -79,7 +79,7 @@ class PortletBase(Location):
                 res = self.template()
             else:
                 res = u''
-        if res:
+        if res and len(self.__schema__):
             return u'<div class="zojax-portlet" kssattr:url="%s">%s</div>'%(self.__url, res)
         else:
             return res
